cd1f0060a9a0cc9364f69ed969d987988c023816
MikeyxBotz <dragonbytexmikey@gmail.com>
2025-07-21 17:29:24 +0530

    Update en.py

--- bot/helper/languages/en.py
+++ bot/helper/languages/en.py
@@ -1,6 +1,6 @@
 START_MSG = """
-This bot can mirror from links|tgfiles|torrents|nzb|rclone-cloud to any rclone cloud, Google Drive or to telegram.
+<b><i>This bot can mirror from links|tgfiles|torrents|nzb|rclone-cloud to any rclone cloud, Google Drive or to telegram.
 Type /{cmd} to get a list of available commands
-"""
-START_BUTTON1 = "Git Repo"
-START_BUTTON2 = "Updates"
+</b></i>"""
+START_BUTTON1 = "Gʀᴏᴜᴘ 🧩"
+START_BUTTON2 = "Uᴘᴅᴀᴛᴇ 🧿"
